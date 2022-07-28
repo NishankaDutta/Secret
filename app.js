e076b7eb409deb31406c1c806084ecca4e8f2164
Nishanka Dutta <nishandutta200@gmail.com>
2022-07-29 00:16:03 +0530

    Level 5 Auth: using passport.js to add cookies and sessions

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
 //jshint esversion:6
 
-require('dotenv').config();
+require('dotenv').config();   // must be at top. Not used in this file currently.
 const express = require("express");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
-const saltRounds = 10;
+const session = require('express-session');
+const passport = require("passport");
+// note: requiring passport-local is not necessary
+const passportLocalMongoose = require("passport-local-mongoose");
+
 
 const app = express();
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
+
+app.use(session({
+  secret: "Our little secret.",   // should be inside an environment variable
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
 
@@ -20,9 +32,14 @@ const userSchema = new mongoose.Schema({
   password: String
 });
 
+userSchema.plugin(passportLocalMongoose);
 
 const User = new mongoose.model("User", userSchema);
 
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function(req, res){
   res.render("home");
@@ -36,46 +53,87 @@ app.get("/register", function(req, res){
   res.render("register");
 });
 
+app.get("/secrets", function(req,res){
+  // The below line was added so we can't display the "/secrets" page
+   // after we logged out using the "back" button of the browser, which
+   // would normally display the browser cache and thus expose the 
+   // "/secrets" page we want to protect. Code taken from this post.
+   res.set('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stal e=0, post-check=0, pre-check=0');
+
+  if(req.isAuthenticated()){
+    res.render("secrets");
+  } else {
+    res.redirect("/login");
+  }
+});
+
+app.get("/logout", function(req,res){
+  req.logout(function(err){
+    if(err){
+      console.log(err);
+    } else {
+        res.redirect("/");
+    }
+  });
+});
+
 
 app.post("/register", function(req, res){
 
-  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-    const newUser = new User({
-      email: req.body.username,
-      password: hash
-    });
-
-    newUser.save(function(err){
-      if(err){
-        console.log(err);
-      } else {
-        res.render("secrets");
-      }
-    });
-  });
+  User.register({username: req.body.username}, req.body.password, function(err, user){
+    if(err){
+      console.log(err);
+      res.redirect("/register");
+    } else {
+      passport.authenticate("local")(req, res, function(){
+        res.redirect("/secrets");
+      })
+    }
+  })
 
 });
 
-app.post("/login", function(req, res){
-  const username = req.body.username;
-  const password = req.body.password;
+// this is the new login route, which authenticates first and THEN
+// does the login (which is required to create the session, or so I
+// understood from the passport.js documentation).
+// A failed login (wrong password) will give the browser error
+// "unauthorized".
 
-  User.findOne({email: username}, function(err, foundUser){
-    if(err) {
+app.post("/login", passport.authenticate("local"), function(req, res){
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password
+  });
+
+  req.login(user, function(err){
+    if(err){
       console.log(err);
     } else {
-      if(foundUser) {
-        bcrypt.compare(password, foundUser.password, function(err, result) {
-          if(result === true) {
-            res.render("secrets");
-          }
-        });
-
-      }
+      res.redirect("/secrets");
     }
   });
-
 });
+
+
+// this is the original login route (with the bug):
+// app.post("/login", function(req, res){
+//
+//   const user = new User({
+//     username: req.body.username,
+//     password: req.body.password
+//   });
+//
+//   req.login(user, function(err){
+//     if(err){
+//       console.log(err);
+//     } else {
+//       passport.authenticate("local")(req, res, function(){
+//         res.redirect("/secrets");
+//       });
+//     }
+//   });
+//
+// });
 
 
 
